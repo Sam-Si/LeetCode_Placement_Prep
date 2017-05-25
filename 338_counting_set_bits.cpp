2dc59d1e9ac3bc359ee321e5febb5833f59742c6
SamSi <sam99xxx1@gmail.com>
2017-05-25 18:04:32 +0530

    Create 338_counting_set_bits.cpp

---
+++ 338_counting_set_bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int helper(int num){
+        int count = 0;
+        while(num > 0){
+            count++;
+            num = (num)&(num-1);
+        }
+        return count;
+    }
+    vector<int> countBits(int num) {
+        vector<int> solution;
+        for(int i = 0;i <= num;i++){
+            int temp;
+            temp = helper(i);
+            solution.push_back(temp);
+        }
+        return solution;
+    }
+};
